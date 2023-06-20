171815d87c441d2447de498ff28e820356af8b29
ARNAB-BOTMAS <arnabmondal203@gmail.com>
2023-06-20 21:15:02 +0530

    first commit

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\data_base\data\venv
+set VIRTUAL_ENV=D:\json_data_sri\test_data_sri\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
